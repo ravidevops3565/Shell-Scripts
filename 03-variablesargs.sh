f0a11b141e2c1bef043f0df3956ec1c6d1a18445
Ravi Reddy <arkr87@gmail.com>
2024-12-29 22:17:14 -0600

    03-variablesargs is missing quotes added

--- 03-variablesargs.sh
+++ 03-variablesargs.sh
@@ -3,5 +3,5 @@
 PERS1=$1
 PERS2=$2
 
-echo "$PERS1: $PERS2, How are you doing? Excited for New Year?
-echo "$PERS2: I'm doing good,Thanks for asking! Ofcourse,super excited for New Year :) "
+echo "$PERS1: $PERS2, How are you doing? Excited for New Year?"
+echo "$PERS2: I'm doing good,Thanks for asking! Ofcourse,super excited for New Year :)"
