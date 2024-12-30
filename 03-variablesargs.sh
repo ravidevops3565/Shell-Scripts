31dc7695f9aee733e98576d070404cd329463684
Ravi Reddy <arkr87@gmail.com>
2024-12-29 22:08:27 -0600

    variables script 03-variablesargs is added

---
+++ 03-variablesargs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PERS1=$1
+PERS2=$2
+
+echo "$PERS1: $PERS2, How are you doing? Excited for New Year?
+echo "$PERS2: I'm doing good,Thanks for asking! Ofcourse,super excited for New Year :) "
