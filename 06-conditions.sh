3f50ec6cb6bdad6e1a918ab448682b4be5020ddf
Ravi Reddy <arkr87@gmail.com>
2025-01-04 19:28:10 -0600

    pushing 06-conditions ss

---
+++ 06-conditions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+date
+USERID=$(id -u)
+
+if [$userid -ne 0]
+then 
+    echo "Please make sure that you are a root user"
+    exit 1
+fi
+
+dnf install mysql -y
+
+if [$?-ne 0]
+then 
+    echo "Mysql Installation....Failure"
+else 
+    echo "Mysql Installation....Success"
+
+dnt install git -y
+
+if [$?-ne 0]
+then
+    echo "Mysql Installation....Failure"
+else 
+    echo "Mysql Installation....Success"
